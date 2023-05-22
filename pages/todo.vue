153e31a8931b87a5f8d80323e35f52ef6b188b37
satinder singh <satindersingh472@gmail.com>
2023-05-23 01:31:42 +0530

    working_with_api_Call

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,15 +1,37 @@
 <template>
+  <div>
     <div>
-        
+        <h1 v-for="(todo,index) in alltodos" :key="index" >All Todos</h1>
+
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import { API } from "aws-amplify";
+import * as queries from "./graphql/queries";
+
+export default {
+  // Simple query
+  methods: {
+    async listAllTodos() {
+      this.allTodos = await API.graphql({ query: queries.listTodos });
+    },
+    async listOneTodo() {
+      this.oneTodo = await API.graphql({
+        query: queries.getTodo,
+        variables: { id: "some id" },
+      });
+    },
+  },
+
+  data() {
+    return {
+      allTodos: undefined,
+      oneTodo: undefined,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
